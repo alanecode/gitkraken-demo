de4247509a9f7d8d898edbf3a7775063415cd538
Andrew Lane <ajlane50@gmail.com>
2019-02-20 22:12:42 +0000

    Add is_great attribute to Person
    Update __repr__ correspondingly

--- people.py
+++ people.py
@@ -1,12 +1,17 @@
 class Person(object):
-    def __init__(self, name):
+    def __init__(self, name, is_great=False):
         self.name = name
+        self.is_great = is_great
 
     def __repr__(self):
-            return self.name + " exists"
+        if self.is_great:
+            return self.name + " is great"
+        else:
+            return self.name + " is okay, I suppose"
 
 p1 = Person("Andrew")
 p2 = Person("Mattia")
 
 for p in [p1, p2]:
     print(str(p))
+
